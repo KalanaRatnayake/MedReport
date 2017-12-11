52db2f12e02da7ed2b6852dbde36dd8f20f511b8
Kalana Ratnayake <kalana.15@cse.mrt.ac.lk>
2017-12-11 22:32:15 +0530

    added the main page

--- app/src/main/java/com/riksolution/medreport/MainActivity.java
+++ app/src/main/java/com/riksolution/medreport/MainActivity.java
@@ -1,7 +1,9 @@
 package com.riksolution.medreport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
