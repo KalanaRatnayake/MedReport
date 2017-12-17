ca8c3378982ceef28c528cf0185eaffaf347927f
Kalana Ratnayake <kalana.15@cse.mrt.ac.lk>
2017-12-18 01:31:40 +0530

    changed doc and patient login pages

---
+++ app/src/main/java/com/riksolution/medreport/DocInputActivity.java
@@ -0,0 +1,13 @@
+package com.riksolution.medreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DocInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_input);
+    }
+}
