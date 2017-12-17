92b2ef0263b049e175971e3b19ad0cead79c638c
Radershan <radershan.15@cse.mrt.ac.lk>
2017-12-17 19:27:17 +0530

    HTTP Post
    With working HTTP post

--- app/src/main/java/com/riksolution/medreport/DoctorRegistrationActivity.java
+++ app/src/main/java/com/riksolution/medreport/DoctorRegistrationActivity.java
@@ -1,18 +1,20 @@
 package com.riksolution.medreport;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,146 +22,68 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-import app.AppConfig;
-import app.AppController;
-import helper.SQLiteHandler;
-import helper.SessionManager;
 
 public class DoctorRegistrationActivity extends AppCompatActivity {
-    private EditText fullname;
-    private EditText regNumber;
-    private EditText userName;
-    private EditText passWord;
 
-    private ProgressDialog pDialog;
-    private SessionManager session;
-    private SQLiteHandler db;
-    private static final String TAG = DoctorRegistrationActivity.class.getSimpleName();
+    private EditText username,password;
+    private Button sign_in_register;
+    private RequestQueue requestQueue;
+    private static final String URL = "http://10.10.26.56/edc/user_control.php";
+    private StringRequest request;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_registration);
 
-        userName = (EditText)findViewById(R.id.etDocUsername);
-        passWord = (EditText)findViewById(R.id.etDocPassword);
+        username = (EditText) findViewById(R.id.etDocUsername);
+        password = (EditText) findViewById(R.id.etDocPassword);
+        sign_in_register = (Button) findViewById(R.id.btnDocRegister);
 
-        // Progress dialog
-        pDialog = new ProgressDialog(this);
-        pDialog.setCancelable(false);
+        requestQueue = Volley.newRequestQueue(this);
 
-        // Session manager
-        session = new SessionManager(getApplicationContext());
-
-        // SQLite database handler
-        db = new SQLiteHandler(getApplicationContext());
-
-
-    }
-
-    public void sendPost(View view) {
-
-        String username = userName.getText().toString().trim();
-        String password = passWord.getText().toString().trim();
-
-        if (!username.isEmpty() && !password.isEmpty()) {
-            registerUser(username, password);
-        } else {
-            Toast.makeText(getApplicationContext(),
-                    "Please enter your details!", Toast.LENGTH_LONG)
-                    .show();
-        }
-    }
-
-    private void registerUser(final String username,
-                              final String password) {
-        // Tag used to cancel the request
-        String tag_string_req = "req_register";
-
-        pDialog.setMessage("Registering ...");
-        showDialog();
-
-        StringRequest strReq = new StringRequest(Request.Method.POST,
-                AppConfig.loginURL, new Response.Listener<String>() {
-
+        sign_in_register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(String response) {
-                //Log.d(TAG, "Register Response: " + response.toString());
-                hideDialog();
+            public void onClick(View view) {
 
-                try {
-                    JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("error");
-                    if (!error) {
-                        // User successfully stored in MySQL
-                        // Now store the user in sqlite
-                        /*String uid = jObj.getString("uid");
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.names().get(0).equals("success")){
+                                Toast.makeText(getApplicationContext(),"SUCCESS "+jsonObject.getString("success"),Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            }else {
+                                Toast.makeText(getApplicationContext(), "Error" +jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
 
-                        JSONObject user = jObj.getJSONObject("user");
-                        String name = user.getString("name");
-                        String email = user.getString("email");
-                        String created_at = user
-                                .getString("created_at");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-                        // Inserting row in users table
-                        db.addUser(name, email, uid, created_at);
 
-                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
-
-                        */// Launch login activity
-                        Intent intent = new Intent(
-                                DoctorRegistrationActivity.this,
-                                DoctorLoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-
-                        // Error occurred in registration. Get the error
-                        // message
-                        String errorMsg = jObj.getString("error_msg");
-                        Toast.makeText(getApplicationContext(),
-                                errorMsg, Toast.LENGTH_LONG).show();
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("username",username.getText().toString());
+                        hashMap.put("password",password.getText().toString());
+
+                        return hashMap;
+                    }
+                };
+
+                requestQueue.add(request);
             }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //Log.e(TAG, "Registration Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_LONG).show();
-                hideDialog();
-            }
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                // Posting params to register url
-                HashMap<String, String> params = new HashMap<String, String>();
-                params.put("username", username);
-                //params.put("email", email);
-                params.put("password", password);
-
-                return params;
-            }
-
-        };
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
-    }
-
-    private void showDialog() {
-        if (!pDialog.isShowing())
-            pDialog.show();
-    }
-
-    private void hideDialog() {
-        if (pDialog.isShowing())
-            pDialog.dismiss();
+        });
     }
 }
+
+
